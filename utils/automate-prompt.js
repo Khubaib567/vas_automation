d1eaf19592b3146046bde3c764842dcfbc846a1e
ahmedkhubaib2226 <ahmedkhubaib2226@gmail.com>
2025-10-08 20:59:57 +0500

    status : encapsulate prompt messages in function

---
+++ utils/automate-prompt.js
@@ -0,0 +1,34 @@
+const {get_live_match,get_past_matches,get_next_matches,get_match_summary} = require('../middleware/match-middleware/get_matches')    
+
+
+
+module.exports = automate_prompt = () => {
+       console.log(`Welcome to FootBall Search Engine
+    1-Get Live Matches 
+    2-Last Match Schedule
+    3-Next Match Schedule
+    4-Get Match Summary`)
+
+    const option = parseInt(prompt('Please select the following options: '))
+
+
+    if(option == 1) {
+        return get_live_match()
+    }
+    if(option == 2) {
+        const league_name = prompt('Enter the league name: ').toLowerCase().replace(' ','-')
+        const date = prompt('Enter the date in given format (yyyy-mm-dd): ').replaceAll(' ','-')
+        return get_past_matches(league_name,date)
+    }
+    else if(option == 3) {
+        const league_name = prompt('Enter the league name: ').toLowerCase().replace(' ','-')
+        const date = prompt('Enter the date in given format (yyyy-mm-dd): ').replaceAll(' ','-')
+        return get_next_matches(league_name,date)
+    }
+    else if(option == 4){
+        const league_name = prompt('Enter the league name: ').toLowerCase().replace(' ','-')
+        const date = prompt('Enter the date in given format (yyyy-mm-dd): ').replaceAll(' ','-')
+        return get_match_summary(league_name,date)
+    }
+    else console.log('System is exited!')
+}
